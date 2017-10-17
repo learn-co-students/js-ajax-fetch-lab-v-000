d4b7e76a3193be2c58d733cbc8ece72a0365a67d
github username <github email address>
2017-10-16 23:19:02 -0500

    Done.

--- index.js
+++ index.js
@@ -12,11 +12,23 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const token = getToken()
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+  	method: "post",
+  	headers: {
+  		Authorization: `token ${token}`
+  	}
+  }).then(res => res.json()).then(json => showForkedRepo(json))
   //use fetch to fork it!
+}
+
+function showForkedRepo(res) {
+	$("#results").append(`<p><a href="${res.html_url}">${res.html_url}</a></p>`)
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  // return "4594012857ff7659a29c9261f7e428b9fd2b7008"
+  return ""
 }
