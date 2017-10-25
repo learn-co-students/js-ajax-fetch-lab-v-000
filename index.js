7891bd079e5e798d4e84dae1c7d702d3f9d35026
Michael Burak <michaelrburak@gmail.com>
2017-10-25 16:05:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const token = ''
+const owner = "MichaelBurak"
+const repobase = "javascript-fetch-lab"
+const api = 'https://api.github.com/'
+const fork = `${owner}/${repobase}`
+
 function getIssues() {
+  fetch(`${api}repos/${fork}/issues`, {
+    method: 'get',
+    headers: {
+    Authorization: `token ${token}`
+  }
+  })
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${api}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+    Authorization: `token ${token}`
+  },
+  body: JSON.stringify(postData)
+  })
 }
 
 function showResults(json) {
@@ -12,7 +34,12 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`${api}repos/${repo}`, {
+   method: 'post',
+   headers: {
+   Authorization: `token ${token}`
+ }
+ })
 }
 
 function getToken() {
