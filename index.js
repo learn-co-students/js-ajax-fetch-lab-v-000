dcccc801b17cc2d95414ad391f774edc4d8c788f
skmcloughlin <skmcloughlin@gmail.com>
2017-11-21 14:25:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 function getIssues() {
+  const urls = document.querySelectorAll("a")
+  const url = urls[1].href + '/issues'
+  const elements = url.split('/')
+  const owner = elements[3]
+  const repo = elements[4]
+  const token = getToken()
+
+  fetch(`https://api.github.com/repos/skmcloughlin/javascript-fetch-lab/issues`, {
+    method: 'get',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json())
+    .then(json => showIssues(json));
+
 }
 
 function showIssues(json) {
+  const issues = []
+  var issue = {}
+  json.forEach(function(element) {
+    document.getElementById("results").innerHTML += '<br>'
+    document.getElementById("results").innerHTML += `Title: ${element.title}    Body:${element.body}`
+    document.getElementById("results").innerHTML += '<br>'
+});
 }
 
 function createIssue() {
+  const urls = document.querySelectorAll("a")
+  const url = urls[1].href + '/issues'
+  const elements = url.split('/')
+  const owner = elements[3]
+  const repo = elements[4]
+  const issueTitle = document.getElementById("title").value
+  const issueBody = document.getElementById("body").value
+  const token = getToken()
+
+  fetch(`https://api.github.com/repos/skmcloughlin/javascript-fetch-lab/issues`, {
+    method: 'post',
+    body: JSON.stringify({
+      "title": `${issueTitle}`,
+      "body": `${issueBody}`
+    }),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(getIssues())
 }
 
-function showResults(json) {
+function showResults(obj) {
+  let url = obj.html_url
+  document.getElementById("results").innerHTML =  '<a href="'+url+'">Link to Forked Repo</a>'
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  const token = getToken()
+
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json())
+    .then(obj => showResults(obj));
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
