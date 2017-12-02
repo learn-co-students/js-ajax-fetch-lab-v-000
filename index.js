6de7d1e9165f1d625c5f7b4b8c07692b0466b272
Sophia Hubeny <shhubeny@gmail.com>
2017-12-02 02:23:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function getIssues() {
+  fetch(`https://api.github.com/repos/ynebuhs/javascript-fetch-lab/issues`, {
+    headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => res.json()).then(json => showIssues);
 }
 
-function showIssues(json) {
+function showIssues(body) {
+ $("#issues").append("<h2>${body.title}</h2> <br><p>${body.body}</p>")
 }
 
 function createIssue() {
+  var title = document.getElementById("title").value
+  var body = document.getElementById("body").value
+  fetch(`https://api.github.com/repos/ynebuhs/javascript-fetch-lab/issues`, {
+    method: 'post',
+    headers: {
+    Authorization: `token ${getToken()}`
+  },
+  body: {
+    "title": title,
+    "body": body
+    }
+}).then(res => res.json()).
+then(getIssues);
 }
+
 
 function showForkedRepo(body) {
   $("#results").html("<a href=`${body.html_url}`>${body.name}</a>")
