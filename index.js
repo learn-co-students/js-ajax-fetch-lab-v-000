777e314bbc1b36c2874ac6992be4538d7b71e10f
burrahey <burra.archana@gmail.com>
2017-12-10 16:34:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
+
 function getIssues() {
+  fetch('https://api.github.com/repos/burrahey/javascript-fetch-lab/issues', {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  document.getElementById("issues").innerHTML = JSON.stringify(json);
 }
 
 function createIssue() {
+  let titleText = document.getElementById("title").value
+  let bodyText = document.getElementById("body").value
+  let issue = {title: titleText, body: bodyText}
+
+  fetch('https://api.github.com/repos/burrahey/javascript-fetch-lab/issues', {
+    method: 'post',
+    body: JSON.stringify(issue),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => getIssues());
 }
 
 function showResults(json) {
+  document.getElementById("results").innerHTML = JSON.stringify(json);
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/' + repo + '/forks', {
+  method: 'post',
+  headers: {
+    Authorization: `token ${getToken()}`
+  }
+  }).then(res => (res.json())).then(json => showResults(json));
+
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+
+  const token = '';
+  return token
 }
