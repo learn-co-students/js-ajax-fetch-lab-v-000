f913ee85e1305e67632058160f4b8c486af83d29
Nicholas D'Amico <nickalan82@icloud.com>
2017-12-13 13:10:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const baseUrl = 'https://api.github.com/';
+const githubOwner = '';
+const forkUrl = `${githubOwner}/javascript-fetch-lab`;
+const repoTitle = 'javascript-fetch-lab';
 function getIssues() {
+	fetch(`${baseUrl}repos/${forkUrl}/issues`).then(res => {
+		res.json().then(json => {
+			for(issue of json) {
+				showIssues(issue);
+			}
+		});
+	})
 }
 
 function showIssues(json) {
+	$('#issues').append(`<ul><h4>${json.title}</h4><li><p>${json.body}</p><p>Created at: ${json.created_at}</p><p>Status: ${json.state}</p></li></ul>`);
 }
 
 function createIssue() {
+	const issueTitle = document.getElementById('title').value;
+	const issueText = document.getElementById('body').value;
+	const owner = githubOwner;
+	fetch(`${baseUrl}repos/${forkUrl}/issues`, {
+		method: 'post',
+		headers: {
+			Authorization: `token ${getToken()}`
+		},
+		body: JSON.stringify({ title: issueTitle, body: issueText })
+	}).then(res => getIssues());
 }
 
 function showResults(json) {
@@ -12,7 +34,21 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const owner = githubOwner;
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+  	method: 'post',
+  	headers: {
+  		Authorization: `token ${getToken()}`
+  	}
+  }).then(res => {
+  	let repo = res;
+  	showForkedRepo(repo);
+  });
+}
+
+function showForkedRepo(repo) {
+	$('#results').html(`<h2>Successfully Forked!</h2><a href="${repo.url}">${repo.url}</a>`);
 }
 
 function getToken() {
@@ -20,3 +56,4 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
