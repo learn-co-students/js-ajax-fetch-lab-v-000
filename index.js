1142a7f9958f9e9736b4ad73ac605480a9a0742a
ndalcin <dalcin.nicole@gmail.com>
2017-12-29 10:01:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-function getIssues() {
+const token = '3adb3c520677c71648a18144424e6bbefc618c72';
+
+function getIssues(json) {
+  fetch('https://api.github.com/repos/ndalcin/javascript-fetch-lab/issues', {
+    method: 'get',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json))
 }
 
+
 function showIssues(json) {
+  let issues = json.map(issue =>{
+    return `<li>${issue.title}: ${issue.body}</li>`
+  })
+
+  let issuesList = `<ul>${issues}</ul>`
+  document.getElementById("issues").innerHTML = issuesList
 }
 
 function createIssue() {
+  let data = {title: document.getElementById('title').value,
+              body: document.getElementById('body').value}
+
+  fetch('https://api.github.com/repos/ndalcin/javascript-fetch-lab/issues',{
+    method: 'post',
+    title: JSON.stringify(data),
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => getIssues(json))
 }
 
-function showResults(json) {
-}
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch('https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showForkedRepo(json));
+}
+
+function showForkedRepo(results){
+  let repoLink = `<a href= ${results.html_url}>Repo</a>`
+  document.getElementById("results").innerHTML = repoLink
 }
 
 function getToken() {
