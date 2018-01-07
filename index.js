cc6a16391b1d2d4861849ad77c4d3f415d426f4a
Andrew <andrew.ramell@gmail.com>
2018-01-07 11:25:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-function getIssues() {
-}
 
-function showIssues(json) {
+
+function getIssues() {
+  fetch('https://api.github.com/repos/ramell86/javascript-fetch-lab/issues/', {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json =>
+    console.log(json)
+)
+  
 }
 
 function createIssue() {
+  // const json = showResults()
+  var title = document.getElementById('title').value
+  var body = document.getElementById('body').value
+  debugger
+  const postData = {title: title, body: body}
+  fetch('https://api.github.com/repos/ramell86/javascript-fetch-lab/issues', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(res => res.json)
+  .then(json => showIssues(json))
+}
+function showIssues(json){
+  debugger
+  $('#issues').append(``)
+
 }
 
 function showResults(json) {
+  var result = document.getElementById('results')
+  debugger
+  result.append(`<a href=${json.url}>${json.url}</a>`)
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-curriculum/javascript-fetch-lab/'
+  fetch('https://api.github.com/repos/' + repo + 'forks', {
+  method: 'post',
+  headers: {
+    Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(json => showResults(json))
+  // debugger
   //use fetch to fork it!
+
+  
 }
 
 function getToken() {
