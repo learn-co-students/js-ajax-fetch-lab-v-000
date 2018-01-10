6530ab369f7459a321f2900d92dcfee6edb17520
Adam Tropp <adt6261@gmail.com>
2018-01-10 06:43:34 +0000

    Done.

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const token = ''//ec03f6938c4eee91f22b704380612333bdad2fba;
+  fetch('api.github.com/repos/' + repo + '/', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
+  // showForkedRepo()
 }
+
+// function showForkedRepo() {
+//   document.getElementById('results').append(json())
+// }
 
 function getToken() {
   //change to your token to run in browser, but set
