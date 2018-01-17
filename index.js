3c166d6af9a2e4e3e6f647c436c73f831f023ce3
Sophia Varella <sophiavarella@gmail.com>
2018-01-16 20:13:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 function getIssues() {
+  const repo = 'sophiav/javascript-fetch-lab';
+  
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  const issueList = json.map(issue => `<li>${issue.title} - ${issue.body}</li>`).join('');
+
+  $('#issues').html(`<ul>${issueList}</ul>`)
 }
 
 function createIssue() {
+  const title = document.getElementById('title').value;
+  const text = document.getElementById('body').value
+  const repo = 'sophiav/javascript-fetch-lab'
+
+  const data = {
+    title: title,
+    body: text
+  }
+
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(data)
+  }).then(() => getIssues())
 }
 
-function showResults(json) {
+// function showResults(json) {
+// }
+
+function showForkedRepo(res) {
+  const link = `<a href="${res.html_url}" >${res.name}</a>`
+  $('#results').append(link)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showForkedRepo(json));
 }
 
 function getToken() {
@@ -20,3 +57,4 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
