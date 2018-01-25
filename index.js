9209d21528aac864b64a663ba7072bc9fe36d70b
DashaDaria <dashadariallc@gmail.com>
2018-01-25 15:54:07 +0800

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
+
 function getIssues() {
+  fetch('https://api.github.com/repos/DashaDaria/javascript-fetch-lab/issues', {
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+  }).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  const details = `<ul>${json.map(j => '<li>' + j.title + ' - ' + j.body + '</li>').join('')}</ul>`
+  $('#issues').append(details);
 }
 
 function createIssue() {
-}
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+  const issueData = {
+    title: title,
+    body: body
+  }
 
-function showResults(json) {
+  fetch('https://api.github.com/repos/DashaDaria/javascript-fetch-lab/issues', {
+    method: 'post',
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+  }).then(getIssues());
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+  method: 'post',
+  headers: {
+    Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showForkedRepo(json));
+}
+
+function showForkedRepo(json){
+  const details = `<a href="${json.html_url}">${json.name}</a>`
+  $('#results').append(details);
 }
 
 function getToken() {
