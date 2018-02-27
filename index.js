4c66d2805f484a8235fddb0a8a879ae34b3bbc96
github username pianistps <pianistps@yahoo.com>
2018-02-27 12:00:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const baseApi = 'https://api.github.com/'
+
 function getIssues() {
 }
 
@@ -8,15 +10,24 @@ function createIssue() {
 }
 
 function showResults(json) {
+  console.log(json)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showResults(json))
 }
+
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  //c2781b17d78121eeceedd44e0a0e301c1261a74a
+  return 'c2781b17d78121eeceedd44e0a0e301c1261a74a'
 }
