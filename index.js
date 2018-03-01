b249f3aeb3e4faa483376283fc2c18ad82301163
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-01 03:24:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 function getIssues() {
+  
+  fetch('https://api/github.com/repos/ff18qd/javascript-fetch-lab/issues', {
+    // headers: {
+    //   Authorization: `token ${token}`
+    // }
+  }).then(res => res.json()).then(json => showIssues()(json));
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  
+  const postData = {
+    // "title": `$("#title").val()`,
+    // "body": `$("#body").val()`
+    "title": "test",
+    "body": "test body",
+    
+  };
+ 
+  const issueURL = 'https://api/github.com/repos/ff18qd/javascript-fetch-lab/issues'
+  fetch(issueURL,  {method: 'post',
+    body: JSON.stringify(postData.body),
+    headers: {
+    'Authorization': `token ${getToken()}`
+  }
+  })
+  .then(resp => resp.json()).then(json => console.log(json));
 }
 
 function showResults(json) {
+  const url = json.html_url
+  // debugger
+  $("#results").append(`<a href="${url}" >Forked Repository</a>`)
 }
 
 function forkRepo() {
-  const repo = '/api.github.com/repos/learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
+  // const repo = "/api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/"
   // const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
   fetch(repo,  {method: 'post',
     headers: {
-    Authorization: `token ${getToken()}`
+    'Authorization': `token ${getToken()}`
   }
   })
-  // .then(resp => resp.json()).then(json => console.log(json));
+  .then(resp => resp.json()).then(json => showResults(json));
+  
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
-  // const token = 'f5c2a844cb5fb2eea5c1259692997a8017988abf';
+  // const token = '9d141e7c67c39a951884ef093b9b46dbb721562d';
   // return token
-  // fetch('https://api.github.com/user/repos', {
-  //   headers: {
-  //     Authorization: `token ${token}`
-  //   }
-  // }).then(res => res.json()).then(json => console.log(json));
+ 
 }
