d9dcac13b4c571d1dacd0224f519dedd32b33d1f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-03-04 11:21:20 -0500

    All but last test passing - index fetch functions fetches the get issues api:

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+const token = "12345"
+const rootUri = "https://api.github.com/repos/"
+const repo = "learn-co-curriculum/javascript-fetch-lab"
+
 function getIssues() {
+  const uri = rootUri + repo + issues
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  const uri = 'javascript-fetch-lab/issues'
+  const postData = document.getElementById('body')
+  fetch(uri, {
+  method: 'post',
+  body: JSON.stringify('test body'),
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => console.log(res));
 }
 
 function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  // const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const uri = rootUri + repo
+  fetch(uri, {
+  method: 'post',
+  // body: JSON.stringify(postData),
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => console.log(res));
   //use fetch to fork it!
 }
 
