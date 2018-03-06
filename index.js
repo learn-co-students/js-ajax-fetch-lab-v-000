433c5b5a4ce21769f2b66dce16a9515d933c99c7
Hannah Carnes <hecarnes@gmail.com>
2018-03-06 00:17:29 -0500

    done

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-function getIssues() {
+const repo = 'learn-co-curriculum/javascript-fetch-lab'
+const myFork = 'hcarnes/javascript-fetch-lab'
+async function getIssues() {
+  let issuesResponse = await fetch(`https://api.github.com/repos/${myFork}/issues`)
+  let issues = await issuesResponse.json()
+
+  showIssues(issues)
 }
 
-function showIssues(json) {
+function showIssues(issues) {
+  for (const issue of issues) {
+    $("#issues").append(`<a href="${issue.html_url}">${issue.title}</a>`)
+  }
 }
 
-function createIssue() {
+async function createIssue() {
+  const issueData = {
+    title: $("#title").val(),
+    body: $("#body").val()
+  };
+  await fetch(`https://api.github.com/repos/${myFork}/issues`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(issueData)
+  })
+  getIssues()
 }
 
-function showResults(json) {
-}
+function showResults(json) {}
 
-function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+async function forkRepo() {
+  let forkRepoResponse = await fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+
+  let jsonResponse = await forkRepoResponse.json()
+
+  $("#results").text(JSON.stringify(jsonResponse))
+  $("#results").prepend(`<a href="${jsonResponse.html_url}">${repo}</a><br>`)
 }
 
 function getToken() {
