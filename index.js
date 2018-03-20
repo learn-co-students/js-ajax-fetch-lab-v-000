370cc5942ca5291d03b1aaaca0d92fa047e5cb1c
Jacqueline <anginblu@gmail.com>
2018-03-20 03:02:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const repoURI = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
-const issueURI = 'https://api.github.com/repos/javascript-fetch-lab/forks'
+const issueURI = 'https://api.github.com/repos/javascript-fetch-lab/issues'
 
 function forkRepo() {
   fetch(repoURI, {
@@ -21,4 +21,34 @@ function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
+}
+
+//issues
+
+
+function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  fetch(issueURI, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify({ title: issueTitle, body: issueBody })
+  }).then(resp => {
+    this.url = resp.url;
+    this.body = resp.body;
+    this.title = resp.title;
+    showIssues(resp);
+  })
+}
+
+//Fetch all issues through the Github API and display / append to the DOM
+
+function getIssues(resp) {
+}
+
+
+function showIssues(data) {
+  $('#issues').append(`<li>Title: <a href="${data.url}">${data.title} </a><br>Body: ${data.body}</li>`)
 }
