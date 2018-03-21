5b1a10b511023501c370765274feca1aae2816df
Autumn Kangas <autumnkangas@gmail.com>
2018-03-21 11:20:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
-function getIssues() {
-}
-
-function showIssues(json) {
-}
-
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`https://api.github.com/repos/AutumnJ/javascript-fetch-lab/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
-function showResults(json) {
+function getIssues(data) {
+  fetch(`https://api.github.com/repos/AutumnJ/javascript-fetch-lab/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          showIssues(data[i]);
+        }
+      } )
+    })
+}
+
+function showIssues(issue) {
+  $('#issues').append(`<li>Title: <a href="${issue.url}">${issue.title} </a><span> | Body: ${issue.body}</span></li>`)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => {
+    // let repo = new Repo(res);
+    showForkedRepo(res);
+  });
 }
 
+function showForkedRepo (res) {
+  $('#results').append(`<h3>Forked Successfully!</h3><a href="${res.url}"> ${res.url}</a>`);
+}
+
+// below is a better way - should abstract logic so its reusable
+// function Repo(attributes){
+//   this.url = attributes.url;
+// }
+
+// Repo.prototype.template = function(){
+//   var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+//   return template;
+// };
+
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
