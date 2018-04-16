6968d221855d02c02d672ec392a182c1658ad8b9
mbalsamo <github email address>
2018-04-16 18:41:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
+const rootURL = 'https://api.github.com/';
+
 function getIssues() {
+  const repo = 'mbalsamo2/javascript-fetch-lab';
+  fetch(`${rootURL}repos/${repo}/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showIssues(json) {
+   $('#issues').append(`<a href="${issue.html_url}">${issue.title}</a><br>`)
 }
 
 function createIssue() {
+  const repo = 'mbalsamo2/javascript-fetch-lab';
+  const issueData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+  fetch(`${rootURL}repos/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(getIssues())
 }
 
 function showResults(json) {
+  let result = `<a href="${json.html_url}">${json.full_name}</a>`;
+  $('#results').html(result)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+
+  fetch(`${rootURL}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(resp => resp.json())
+  .then(json => showResults(json));
 }
 
 function getToken() {
