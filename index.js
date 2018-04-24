b8b99786768cceb223b7e7500ae7bb84ef6fdb79
github username <github email address>
2018-04-24 11:54:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-function getIssues() {
+function getIssues(data) {
+  fetch('https://api.github.com/repos/moresmiles/javascript-fetch-lab/issues').
+  then(resp => {
+  resp.json().then( data => {
+    for (let i = 0; i < data.length; i++){
+      showIssues(data[i]);
+    }
+  })
+})
 }
 
-function showIssues(json) {
+function showIssues(issue) {
+  $('#issues').append(`<li>Title: <a href="${issue.url}">${issue.title} </a><span> | Body: ${issue.body}</span></li>`)
 }
 
 function createIssue() {
+  //post /repos/:owner/:repo/issues
+  const titleData = document.getElementById('title').value
+  const bodyData = document.getElementById('body').value
+  const issueData = {title: titleData, body: bodyData}
+  fetch('https://api.github.com/repos/moresmiles/javascript-fetch-lab/issues', {
+    method: 'post',
+    headers: {
+      Authorization: getToken()
+    },
+    body: JSON.stringify(issueData)
+  }).then(res => getIssues());
 }
 
 function showResults(json) {
+  $('#results').html(`<div> <a href="${json.html_url}"> ${json.html_url} </a> </div>`)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  // /repos/:owner/:repo/forks
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: getToken()
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
