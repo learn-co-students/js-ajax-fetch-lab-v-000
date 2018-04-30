9f2284fee73e51e241a31c4a4e0aeb541a1379a6
ingridwong0715 <ingrid830715@gmail.com>
2018-04-30 19:58:24 -0400

    Done.

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ function createIssue() {
 }
 
 function showForkedRepo(json) {
-  debugger;
+
 
 
 }
 
 function forkRepo() {
 
-  //const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-curriculum/javascript-fetch-lab'
 
-  fetch('https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks', {
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
     method: 'post',
     headers: {
-      Authorization: 'token bda7ca5c8863978cb4b8f4fd1fec9c2c0c36f473'
+      Authorization: `token getToken()`
     }
   }).then(function(res){
-
-    const json = res.json()
     debugger;
+    const json = res.json()
+
       showForkedRepo(json)
   })
 }
@@ -34,5 +34,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'bda7ca5c8863978cb4b8f4fd1fec9c2c0c36f473'
+  return ''
 }
