2eef5a2b88e58272b9a2c56107fc9f4469a19a3f
Jennifer Dobak <jdobak1@gmail.com>
2018-05-17 15:34:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// const token = '6730dd407cd98672f10c922d0372ddc172fbae5f'
+
 function getIssues() {
 }
 
@@ -13,10 +15,21 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => console.log(res))
+}
+
+function showForkedRepo() {
+  $('#results').append()
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '6730dd407cd98672f10c922d0372ddc172fbae5f'
+  // return ''
 }
