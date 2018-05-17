27ae34b0a50cca810e811800e0929577c9a5bd1e
jendobes <jdobak1@gmail.com>
2018-05-17 12:39:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // const token = '6730dd407cd98672f10c922d0372ddc172fbae5f'
 
-function getIssues() {
+function getIssues(json) {
+  const repo = 'jendobes/javascript-fetch-lab'
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: 'get',
+    headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  const issuesList = "<ul>" + json.map(issue => `<li>Title: ${issue.title}<br>Body: ${issue.body}</li>`).join('') + "</ul>"
+  $('#issues').append(issuesList)
 }
 
 function createIssue() {
+  const repo = 'jendobes/javascript-fetch-lab'
+  let titleData = document.getElementById('title').value
+  let bodyData = document.getElementById('body').value
+  let data = {title: titleData, body: bodyData}
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => getIssues())
 }
+
 
 function showResults(json) {
 }
@@ -20,16 +41,15 @@ function forkRepo() {
     headers: {
     Authorization: `token ${getToken()}`
   }
-}).then(res => console.log(res))
+}).then(res => res.json(res)).then(json => showForkedRepo(json))
 }
 
-function showForkedRepo() {
-  $('#results').append()
+function showForkedRepo(json) {
+  $('#results').append(`<a href=${json.svn_url}> ${json.svn_url}</a>`)
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '6730dd407cd98672f10c922d0372ddc172fbae5f'
-  // return ''
+  return ''
 }
