290029fcfbd7a7b707cde6b0a5b209f98c4b29d3
catalanowebdevelopment <catalano.work@gmail.com>
2018-06-06 12:50:25 -0400

    Done.

--- index.js
+++ index.js
@@ -7,16 +7,30 @@ function showIssues(json) {
 function createIssue() {
 }
 
-function showResults(json) {
-}
-
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+      method: 'post',
+      headers: {
+          Authorization: `token ${getToken()}`
+      }
+  }).then(res => res.json()).then(showForkedRepo)
+}
+
+function showForkedRepo(response) {
+    console.log(response)
+    $('#results').html(template => { return(`
+        <div>
+            <h4>URL: <a href="${response.clone_url}">${response.name}</a></h4>
+            <p>Created At: ${response.created_at}</p>
+            <p>Forks: ${response.forks}</p>
+            <p>Forks Count: ${response.forks_count}</p>
+        </div>
+    `)})
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return 'b74f2295bbf1dd78f010be3f498a2745f10bb373'
 }
