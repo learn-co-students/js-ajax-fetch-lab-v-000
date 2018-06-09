ed9ae0935359b23660cbb660730063d0e8215877
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-09 14:39:18 +0100

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
+const base = 'https://api.github.com'
+const username = 'safuya'
+const lab = 'javascript-fetch-lab'
+
 function getIssues() {
+  fetch(`${base}/repos/${username}/${lab}/issues`, {
+    headers: { Authorization: `token: ${getToken()}` }
+  }).then(res => res.json())
+    .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  console.log(json)
 }
 
 function createIssue() {
+  const title = document.getElementById('title').value
+  const body = document.getElementById('body').value
+  fetch(`${base}/repos/${username}/${lab}/issues`, {
+    method: 'post',
+    headers: { Authorization: `token ${getToken()}` },
+    body: JSON.stringify({ title, body })
+  }).then(_ => getIssues())
 }
 
 function showResults(json) {
+  console.log(json)
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  return fetch(`${base}/repos/learn-co-curriculum/${lab}/fork`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json)
+    .then(json => showResults(json))
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
