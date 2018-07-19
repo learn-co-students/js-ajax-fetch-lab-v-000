0d01356c5d0fee2df164daf60209ecd00ec0aea2
AspenJames <aspenjames@tqca.org>
2018-07-19 09:55:02 -0700

    save progress

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 function getIssues() {
+  let repo = $("#results a")[0].innerHTML;
+  fetch(`https://api.github.com/repos/${repo}/issues`).
+    then(result => result.json()).
+    then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  let issues = json.map(issue => {
+    return `<div>
+      <h3><a href="${issue.user.html_url}">${issue.user.login}</a> - <a href="${issue.html_url}">${issue.title}</a></h3>
+      <p>${issue.body}
+    </div>`
+  }).join('');
+
+  $("#issues").html(issues);
 }
 
 function createIssue() {
-}
-
-function showResults(json) {
+  let issueData = {
+    title: $("#title")[0].value,
+    body:  $("#body")[0].value
+  };
+  let repo = $("#results a")[0].innerHTML;
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${getToken()}`,
+    }
+  }).then(res => getIssues());
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showForkedRepo(json));
   //use fetch to fork it!
+}
+
+function showForkedRepo(json) {
+  let repo = `
+    <h3>Forked Successfully!</h3>
+    <a href="${json.html_url}">${json.full_name}</a>
+  `;
+  $("#results").html(repo);
 }
 
 function getToken() {
