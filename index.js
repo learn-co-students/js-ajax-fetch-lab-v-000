661256bd785f8cc4dede1bba206d0b4295e080f2
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-29 13:53:51 -0400

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,36 @@
 const rootApi = "https://api.github.com/"
 const userName = ""
 const fork = `${userName}/javascript-fetch-lab`
+const repo = 'learn-co-curriculum/javascript-fetch-lab'
 
-
-function getIssues() {
+function Issue(response){
+  this.title = response.title;
+  this.body = response.body;
+  this.url = response.url
+  this.pageAddition = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
 }
 
-function showIssues(json) {
+function getIssues() {
+  fetch(`${rootApi}repos/${fork}/issues`)
+    .then(data => data.map(d)(showIssues(new Issue(d[i]))))
+}
+
+function showIssues(data) {
+  $('#issues').append(data.pageAddition)
 }
 
 function createIssue() {
+  let isTitle = document.getElementById('title').value
+  let isBody = document.getElementById('body').value
+  fetch(`${rootApi}repos/${fork}/issues`,
+  {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify({title: isTitle, body: isBody})
+  }).then(res => console.log(res))
+    // getIssues())
 }
 
 function showResults(json) {
@@ -18,7 +39,7 @@ function showResults(json) {
 
 function Repo(response) {
 this.url = response.url
-this.pageAddition = `<h2> Forked Sucessfully! <a href="this.url"> ${this.url}</a></h2>`
+this.pageAddition = `<h2> Forked Sucessfully! <a href="${this.url}"> ${this.url}</a></h2>`
 }
 
 
@@ -26,7 +47,7 @@ function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
   fetch(`${rootApi}repos/${repo}/forks`, {
-    method: 'POST',
+    method: 'post',
     headers: {
       Authorization: `token ${getToken()}`
     }
@@ -44,7 +65,7 @@ function showForkedRepo(repo) {
 }
 
 function getToken() {
-   return '5c4fcba27e0e29e1ff73d52f8c722c76b45bb6c1'
+    return ''
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
 
