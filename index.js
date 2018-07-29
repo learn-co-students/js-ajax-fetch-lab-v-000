a20da5abd131b2519eb19b9b232513c2664e1725
katleiahramos <katleiahramos@gmail.com>
2018-07-29 12:02:39 -0500

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,36 @@ function showIssues(json) {
 }
 
 function createIssue() {
+ const issueTitle = $('#title').val()
+ const issueBody = $('#body').val()
+
+
 }
 
 function showResults(json) {
 }
 
+function Repo(attributes){
+  this.url = attributes.url;
+}
+
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(resp => {
+
+    let repo = new Repo(resp);
+    showRepo(repo)
+  })
+}
+
+function showRepo(repo) {
+  $('#results').append(`<li>Successfully Forked: <a href="${repo.url}">${repo.url}</a></li>`)
 }
 
 function getToken() {
