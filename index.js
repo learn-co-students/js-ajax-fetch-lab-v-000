a2c39ce365fc5c9d63f811f6b4fa040692fc3bd9
Dee Race <deerace12@gmail.com>
2018-08-14 19:59:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 function getIssues() {
+  fetch(`https://api.github.com/repos/javascript-fetch-lab/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+    .then(json => console.log(json));
 }
 
 function showIssues(json) {
+  $("#issues").html(json => getIssues())
 }
 
 function createIssue() {
-}
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+
+  const postData = {
+    title: title,
+    body: body
+  }
+
+  fetch(`https://api.github.com/repos/javascript-fetch-lab/issues`, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+};
 
 function showResults(json) {
+  $("#results").html(forkRepo())
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
-}
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+};
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  const token = ''
+  return token
 }
