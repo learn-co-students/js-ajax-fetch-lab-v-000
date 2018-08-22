8d0457930812b2a5a8a55ad03e0c78a0ef386935
paaaarv <pkumar@trinity.edu>
2018-08-22 16:27:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function getIssues() {
-}
+  fetch("https://api.github.com/repos/paaaarv/javascript-fetch-lab/issues", {
+    headers:{
+    Authorization: `token: ${getToken()}`}
+    });
+  }
+
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  const issueBody = {
+    body: "test body"
+  }
+  fetch("https://api.github.com/repos/paaaarv/javascript-fetch-lab/issues",{
+    method: "post",
+    body: JSON.stringify(issueBody),
+    headers: {
+      Authorization: `token: ${getToken()}`
+
+    }
+  })
 }
 
 function showResults(json) {
@@ -13,6 +29,12 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}`, {
+    method: "post",
+    headers: {
+      Authorization: `token: ${getToken()}`
+    }
+  })
 }
 
 function getToken() {
