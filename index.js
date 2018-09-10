02c9dc1fe2d563c63c5ed2f713ac1a43dd363c07
Michele Conway <mconway890@gmail.com>
2018-09-10 16:24:06 -0400

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return '';
+
+  //const token = 'YOUR_TOKEN_HERE';
+
+  //fetch('https://api.github.com/user/repos', {
+  //    headers: {
+  //      Authorization: `token ${token}`
+  //    }
+  //  })
+  //  .then(res => res.json())
+  //  .then(json => console.log(json));
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab')
+    .then(resp => resp.json())
+    .then(json => console.log(json));
 }
 
 function showResults(json) {
