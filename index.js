f4b2a8dec7ff806249b7e9a0eee1cdcad6a7c301
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-13 01:08:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return 'cca66f75ed58157beed6548cebe5597b289311ae';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(
+    `https://api.github.com/repos/${repo}/forks`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(res => console.log(res));
 }
 
 function showResults(json) {
