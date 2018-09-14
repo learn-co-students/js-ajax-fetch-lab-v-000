8a650be755e111f697e841c72ed855869065de11
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-14 01:50:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
+const githubURI = 'https://api.github.com/repos';
+const repoName = 'js-ajax-fetch-lab';
+
+var user;
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'cca66f75ed58157beed6548cebe5597b289311ae';
+  return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const learn = 'learn-co-curriculum';
+  console.log("Going to fork repo");
   fetch(
-    `https://api.github.com/repos/${repo}/forks`,
+    `${githubURI}/${learn}/${repoName}/forks`,
     {
       method: 'POST',
       headers: {
         Authorization: `token ${getToken()}`
       }
     }
-  ).then(res => console.log(res));
+  ).then(resp => resp.json())
+  .then( json => showResults(json));
 }
 
-function showResults(json) {
-  //use this function to display the results from forking via the API
+function showResults(resp) {
+  console.log("show results",resp);
+  let link=`<a href=${resp.html_url}>${resp.full_name}</a>`;
+  user = resp.owner.login;
+  document.getElementById('results').innerHTML = link;
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const issueData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+  console.log('create issue',issueData);
+  fetch(
+    `${githubURI}/${user}/${repoName}/issues`,
+    {
+      method: 'POST',
+      body: JSON.stringify(issueData),
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(resp => getIssues(resp))
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch(
+    `${githubURI}/${user}/${repoName}/issues`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(res => res.json())
+    .then(json => console.log(json));
 }
+
+
+// function showIssues(issues) {
+//   console.log ("show issues",resp);
+//   let html = '<ul>';
+//   issues.forEach ( function (issue) {
+//     html += `<li>${issue.title}<li>`
+//   })
+//   html += '<ul>';
+//   document.getElementById('issues').innerHTML = html;
+// }
