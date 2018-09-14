f9ac679581dc4549a5dbd46d04093f4244e55e09
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-14 15:49:34 +0000

    Done.

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com//repos/${repo}/forks`, {
+    method: 'POST'
+    headers: {Authorization: `token ${getToken()}`
+  }).then(resp => resp.json()).then(json=> showResults(json))
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  $('#results').html(
+    json.html_url
+  )
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+
 }
 
 function getIssues() {
