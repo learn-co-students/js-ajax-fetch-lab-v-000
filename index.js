5418736b0109255956fc270349131d129cad45eb
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-14 19:02:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
+const user = 'emilyyz92';
+const repoName = 'js-ajax-fetch-lab'
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return '03980af6437f1ee711534baf88aaa01efba79d01';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
-  fetch(`https://api.github.com//repos/${repo}/forks`, {
-    method: 'POST'
-    headers: {Authorization: `token ${getToken()}`
-  }).then(resp => resp.json()).then(json=> showResults(json))
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {Authorization: `token ${getToken()}`}
+  }).then(resp => resp.json()).then(json => showResults(json))
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
-  $('#results').html(
-    json.html_url
-  )
+  document.getElementById('results').innerHTML =
+    `<a href="${json.html_url}>"${repoName}</a>`
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
-
+  const issues = {}
+  issues.title = document.getElementById('title').value
+  issues.body = document.getElementById('body').value
+  fetch(`https://api.github.com//repos/${user}/${repoName}/issues`,
+    {
+    method: 'POST',
+    body: `${json.stringify(issues)}`,
+    headers: {Authorization: `token ${getToken()}`}
+  }).then(resp => resp.json()).then(json => getIssues(json))
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch(`https://api.github.com//repos/${user}/${repoName}/issues`,
+    {
+      method: 'POST',
+      headers: {Authorization: `token ${getToken()}`}
+    }
+  ).then(resp => resp.json()).then(json => showIssues(json))
+}
+
+function showIssues(json) {
+  document.getElementById('issues').innerHTML =
+    `<ul>` + JSON.parse(json).map(
+      issue =>
+      '<li>' +
+      `<a href="${issue.html_url}">${issue.title}</a>`
+      + '</li>').join()
 }
