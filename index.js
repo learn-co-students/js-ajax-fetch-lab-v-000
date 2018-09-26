66ab02cf262d831e39a1666444ccb1476bb78b03
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-26 16:20:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const token = 'f42ca8c18bb6bad493c208a3a990f742df060c97'
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+
+  return token;
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+  .then(response => response.json())
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
