2c7f35af46261b9527bfee09b057a029b7e34b98
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-27 17:54:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,1 @@
-const token = '8b051c688662938d9f82506cd11e18305562d149'
-
-
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return token;
-}
-
-function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
-  fetch(`https://api.github.com/repos/${repo}/forks`, {
-    method: 'POST',
-    headers: {
-      Authorization: `token ${token}`
-    }
-  })
-  .then(response => response.json())
-  .then(json => showResults(json));
-}
-
-function showResults(json) {
-  document.getElementById("results").innerHTML = `<a href="${json.html_url}" target="_blank">"${json.html_url}"</a>`
-}
-
-function createIssue() {
-  let title = document.getElementById("title").value
-  let body = document.getElementById("body").value
-  
-}
-
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
-}
+const token = ''function getToken() {  //change to your token to run in browser, but set  //back to '' before committing so all tests pass  return token;}function forkRepo() {  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';  //use fetch to fork it!  fetch(`https://api.github.com/repos/${repo}/forks`, {    method: 'POST',    headers: {      Authorization: `token ${token}`    }  })  .then(response => response.json())  .then(json => showResults(json));}function showResults(json) {  document.getElementById("results").innerHTML = `<a href="${json.html_url}" target="_blank">"${json.html_url}"</a>`}function createIssue() {  let title = document.getElementById("title").value  let body = document.getElementById("body").value  let issue = {title: title, body: body}  fetch(`https://api.github.com/repos/awilmerding1/js-ajax-fetch-lab/issues`, {    method: 'POST',    body: JSON.stringify(issue),    headers: {      Authorization: `token ${token}`    }  })  .then(response => response.json())  .then(json => getIssues(json));}function getIssues() {  //once an issue is submitted, fetch all open issues to see the issues you are creating  fetch(`https://api.github.com/repos/awilmerding1/js-ajax-fetch-lab/issues`, {    headers: {      Authorization: `token ${token}`    }  })  .then(response => response.json())  .then(json => document.getElementById("issues").innerHTML = json.map(function(issue) {      return `<ul><li>${issue.title} - ${issue.body}</li></ul>`  }));}
