36f81de59775f79061c544ee8ac5afd2c278a945
mstmari <michael.landrum.st.marie@gmail.com>
2018-10-11 22:57:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
+const baseURL = '/api.github.com';
+const user = 'mstmari';
+const token = ''
+const repo = 'js-ajax-fetch-lab';
+
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return token;
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  // POST /repos/:owner/:repo/forks
+  const userName = 'learn-co-curriculum';
+
+  const url = `${baseURL}/repos/${userName}/${repo}/forks`
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+      Authorization: `token ${getToken()}`
+    }
+    })
+  .then(val => val.json())
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href= ${user}/${json.html_url}>${json.name}</a>`
+
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const title = document.getElementById('title').value
+  const body = document.getElementById('body').value
+  //From github API  POST /repos/:owner/:repo/issues
+  let data = {
+    title: title,
+    body: body
+  }
+  const url = `${baseURL}/repos/${user}/${repo}/issues`
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`,
+      'Content-Type': "application/json"
+
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json())
+    .then(json => console.log(json))
+    .then(json => getIssues());
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+
+  // GET /repos/:owner/:repo/issues
+  const url = `${baseURL}/repos/${user}/${repo}/issues`
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${getToken()}`,
+    }
+  }).then(res => res.json())
+    .then(json => document.getElementById('results').innerHTML = json.title);
+// document.getElementById('results').innerHTML = json.title
 }
