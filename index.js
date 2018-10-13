7ca675a7ca12f7c471c3cd8135e67ff3d4aa1b0f
Dan Suarez <suarez.daniel101@gmail.com>
2018-10-13 02:21:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  const token = '2dbcc248c96cc84a6fa1f02b63f79a9caf92ae0a';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
   return '';
 }
 
