43141dac9faa5832cef71a90b68d11a0c4632171
Dan Suarez <suarez.daniel101@gmail.com>
2018-10-18 01:41:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  const token = '2dbcc248c96cc84a6fa1f02b63f79a9caf92ae0a';
+  const token = '';
   return '';
 }
 
