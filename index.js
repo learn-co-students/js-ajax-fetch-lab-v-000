c36f9c86a7d78feb0d5e7eb1efe3f437f7414f6c
github username <github email address>
2018-10-21 18:53:09 -0700

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,45 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const uri = `https://api.github.com/repos/${repo}/forks`
+  const token = getToken()
+  fetch(uri, {
+   method: 'POST',
+   headers: {
+     Authorization: `token ${getToken}`
+    }
+   })
+  .then(res => res.json())
+  .then(json => console.log(json));
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  let json = forkRepo()
+  return json
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = 'kritirai/js-ajax-fetch-lab';
+  const uri = `https://api.github.com/repos/${repo}/issues`;
+  const issueTitle = document.getElementById("title").value;
+  const issueBody = document.getElementById("body").value;
+
+  fetch(uri, {
+    method: 'POST',
+    title: title,
+    body: body,
+    headers: {
+      Authorization: `token ${getToken}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = 'kritirai/js-ajax-fetch-lab';
+  const uri = `https://api.github.com/repos/${repo}/issues`;
+
+  fetch(uri).then(res => res.json()).then(json => console.log(json));
 }
