861b6b0eb7318f12b5e8258693b89485c0bbe5df
Hector-Ariceaga <crablogni@hotmail.com>
2018-12-02 11:41:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function getToken() {
+  const token = '76e4bcd76b91f6d3372dd2c5e71e9177a6573b2a'
+  return token;
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  // return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => console.log(res))
   //use fetch to fork it!
 }
 
@@ -20,3 +28,4 @@ function createIssue() {
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
 }
+
