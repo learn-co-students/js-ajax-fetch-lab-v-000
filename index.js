53b132f75ed83104e1bc1fbea7bda80236096b74
github username <github email address>
2018-12-12 17:54:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const baseURL = 'https://api.github.com';
+const user = 'SallyZ12'
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  const token = '1c13bb04695ac045d29a99daf0ea6be075d26e2f';
+  const token = '';
 
-// fetch('https://api.github.com/user/repos', {
-//   headers: {
-//     Authorization: `token ${token}`
-//   }
-// })
-//   .then(res => res.json())
-//   .then(json => console.log(json));
   return token;
-  // return '';
 }
 
 function forkRepo() {
@@ -34,13 +27,42 @@ function showResults(json) {
   //use this function to display the results from forking via the API
   document.getElementById('results').innerHTML =
   `<a href= ${json.html_url}>${json.html_url}</a>`;
-  console.log(json)
+  console.log('showResults',json)
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  const postData = {title: document.getElementById('title').value,
+  body:document.getElementById('body').value};
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(response=>response.json())
+  .then(json=>getIssues());
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  fetch(url,{
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(response=>response.json())
+
+    .then(json=>
+      {document.getElementById('issues').innerHTML = "<ul>" + json.map(j =>
+        `<li> ${j.title} <br>
+         ${j.body}
+         </li>`).join('') + "</ul>";
+       });
 }
