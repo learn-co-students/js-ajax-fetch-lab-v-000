b2df17293aeb0b36f873507f34e3216c5f7eeb34
github username <github email address>
2018-12-12 14:34:23 -0500

    work in process - starting createIssue()

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+const baseURL = 'https://api.github.com';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  const token = '1c13bb04695ac045d29a99daf0ea6be075d26e2f';
+
+// fetch('https://api.github.com/user/repos', {
+//   headers: {
+//     Authorization: `token ${token}`
+//   }
+// })
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+  return token;
+  // return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(response=>response.json())
+    .then(json=>showResults(json))
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML =
+  `<a href= ${json.html_url}>${json.html_url}</a>`;
+  console.log(json)
 }
 
 function createIssue() {
