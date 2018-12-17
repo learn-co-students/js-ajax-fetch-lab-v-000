ec15b2a9ac15d03f7967d0facf704c31965fcbe8
AAM77 <Adeel.A.Mhd@gmail.com>
2018-12-17 18:40:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+let resultsDiv = document.getElementById('results');
+let issuesDiv = document.getElementById('issues');
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -6,17 +10,58 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  )
+  .then(response => response.json())
+  .then(json => showResults(json))
 }
 
+//use this function to display the results from forking via the API
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  resultsDiv.innerHTML = `<a href="${json.html_url}" target="_blank">JS AJAX Fetch Lab</a>`;
 }
 
+//use this function to create an issue based on the values input in index.html
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const forkedRepo = 'AAM77/js-ajax-fetch-lab'
+  let postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  }
+
+  fetch(`https://api.github.com/repos/${forkedRepo}/issues`,
+  {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  })
+  .then(response => response.json())
+  .then(getIssues());
+
+  document.getElementById('title').value = "";
+  document.getElementById('body').value = "";
 }
 
+//once an issue is submitted, fetch all open issues to see the issues you are creating
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch('https://api.github.com/repos/AAM77/js-ajax-fetch-lab/issues')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(issue => {
+      issuesDiv.innerHTML +=
+       `<ul>
+          <li> Issue Number: ${issue.number} </li>
+          <li> Issue Title: ${issue.title} </li>
+          <li> Issue Text: ${issue.body} </li>
+        </ul>`
+    });
+  });
 }
