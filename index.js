3e0ea77e1e9a0b0d5838d0b522479d61e99e9087
Hillevi Eklöw <hillevieklow@gmail.com>
2018-12-30 11:36:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
+let ownerRepo = 'hillevieklow/js-ajax-fetch-lab';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  // const token = '41f1c60feb4036fb5ac8243ea10bc44e987a63bf';
   return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  const url = 'https://api.github.com/repos/' + repo + '/forks';
-  fetch(url, { method: 'POST', headers: {Authorization: 'token' + getToken()} })
-    .then(res => res.json())
-    .then(json => showResults(json));
+
+  fetch('https://api.github.com/repos/' + repo + '/forks', {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
-  const results = document.getElementById('results').innerHTML;
-  results = '<a href="' + json.html_url + '">' + json.html_url + '</a>';
+  //Append this link to the results div
+  const forkedRepo = '<a href="' + json.html_url + '">New Repo</a>';
+  ownerRepo = json.full_name;
+  document.getElementById('results').innerHTML = forkedRepo;
 }
 
 function createIssue() {
-  const repo = 'xplor8r/js-ajax-fetch-lab';
-  const url = 'https://api.github.com/repos/' + repo + '/issues';
-  const issueTitle = document.getElementById('title').value;
-  const issueBody = document.getElementById('body').value;
-  const issueData = {title: issueTitle, body: issueBody};
-  fetch(url, { method: 'POST', body: JSON.stringify(issueData), headers: {Authorization: 'token' + getToken()} })
-    .then(res => res.json())
-    .then(json => getIssues());
+  const issueData = {
+    title: title.value,
+    body: body.value
+  };
+
+  fetch('https://api.github.com/repos/' + ownerRepo + '/issues', {
+    method: 'POST',
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  });
+
+  getIssues();
 }
 
 function getIssues() {
-  const repo = `xplor8r/js-ajax-fetch-lab`;
-  const url = 'https://api.github.com/repos/' + repo + '/issues';
-  fetch(url, { headers: {Authorization: 'token' + getToken()} })
-    .then(res => res.json())
-    .then(json => console.log(json));
+  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch('https://api.github.com/repos/' + ownerRepo + '/issues', {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(function(json) {
+    const issues = json;
+    const issuesList = `<ul>${issues
+      .map(
+        issue =>
+          '<li><strong>' +
+          issue.title +
+          '</strong> - ' +
+          issue.body +
+          '</li>'
+      ).join('')}</ul>`;
+    document.getElementById('issues').innerHTML = issuesList;
+  });
+}
