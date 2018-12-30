539a7b4a21cbacf51a6a6bb3ff99c0ff5b5efe2f
Great_Divide <bbirdwell+git@gmail.com>
2018-12-30 12:52:44 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let resultsDiv = document.getElementById('results')
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -7,10 +9,22 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, 
+  // POST /repos/:owner/:repo/forks
+  	{
+  		method: 'POST',
+  		headers: {
+  			Authorization: `token ${getToken()}`
+  		}
+  	}
+  ).then(res => res.json()).then(json => showResults(json))
+  // ).then(res => res.json()).then(json => console.log(json));
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  const forkLink = json.html_url
+  resultsDiv.innerHTML = `<a href="${forkLink}>JS AJAX Fetch Lab</a>`;
 }
 
 function createIssue() {
