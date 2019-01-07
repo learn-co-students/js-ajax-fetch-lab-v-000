890dc8902f0450938716be93c34a57ec0e05c089
micahshute <micah.shute@gmail.com>
2019-01-07 12:04:26 -0600

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,75 @@ function getToken() {
   return '';
 }
 
+function getHeader(){
+  return { Authorization: `token ${getToken()}` }
+}
 function forkRepo() {
+  const prefix = 'https://api.github.com/repos/';
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const postscript = '/forks';
+  const path = prefix + repo + postscript;
+  fetch(path, {
+    method: 'post',
+    headers: getHeader()
+  }).then(res => res.json())
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  console.log(json)
+  let html = `<p><strong id="repo-name">${json.name}</strong> :: <span id="owner">${json.owner.login}</span></strong></p><a href="${json.html_url}" target="_">Your Fork</a>`
+  document.querySelector('#results').innerHTML = html;
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const title = document.querySelector('#title').value;
+  const content = document.querySelector('#body').value;
+  const body = JSON.stringify({ title: title, body: content });
+  const path = `https://api.github.com/repos/${getOwner()}/${getRepo()}/issues`;
+  fetch(path, {
+    method: 'post',
+    headers: getHeader(),
+    body: body
+  })
+  .then(res => getIssues());
+}
+
+function getOwner(){
+  let owner
+  if(owner = document.querySelector('#owner')){
+    return owner.innerHTML;
+  }else{
+    return "micahshute";
+  }
+
+}
+
+function getRepo(){
+  let repo
+  if(repo = document.querySelector('#repo-name')){
+    return repo.innerHTML;
+  }else{
+    return "js-ajax-fetch-lab"
+  }
+  
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+console.log("HERE")
+ const path = `https://api.github.com/repos/${getOwner()}/${getRepo()}/issues`
+ fetch(path, {
+   headers: getHeader()
+ }).then(res => res.json())
+ .then(json => showIssues(json))
+}
+
+function showIssues(json){
+  console.log("SHOW ISSUES")
+  console.log(json);
+  let html = "<ul>" + json.map(j => (
+    `</li><h4>${j.title}</h4><p>${j.number} :: <span id="body">${j.body} :: </span><a href="${j.url}">${j.url}</a></p></li>`
+  )).join('') + "</ul>";
+  document.querySelector('#issues').innerHTML = html;
 }
