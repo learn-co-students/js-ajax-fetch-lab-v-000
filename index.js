8870276d9274a3bc561be2ff6ef3ac64e6f5092d
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-11 07:36:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const base = 'https://api.github.com/repos';
+const user = 'FKRauber';
+const learnRepo = 'learn-co-curriculum/js-ajax-fetch-lab';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -5,18 +9,62 @@ function getToken() {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const url = `${base}/${learnRepo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(result => result.json())
+  .then(json => showResults(json));
+}
+
+function forkRepo() {
+  const url = `${base}/${learnRepo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.htmml_url}</a>`;
 }
+
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${base}/${repo}/issues`;
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(result => result.json())
+  .then(json => getIssues());
 }
 
+
+
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${base}/${repo}/issues`;
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(result => result.json())
+  .then(json => console.log(json));
 }
