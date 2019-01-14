6da72bfa999efb25c71553a9e4ef61415ff57472
kachel wittig <kjwittig@gmail.com>
2019-01-14 13:25:35 -0600

    Donezo:

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const repo = 'js-ajax-fetch-lab'
+const owner = 'learn-co-curriculum'
+const user = "scrptktty"
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -5,18 +9,42 @@ function getToken() {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  // POST /repos/:owner/:repo/forks
+  fetch(`https://api.github.com/repos/${owner}/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    },
+  }).then(response => response.json())
+    .then(json => showResults(json))
+
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML =
+    `<a href="${json.html_url}">Repo</a>`
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  // POST /repos/:owner/:repo/issues
+  const postData = { body: 'test body'}
+  fetch(`https://api.github.com/repos/${user}/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify(postData)
+    headers: {
+      Authorization: `token ${token}`
+    },
+  }).then(response => response.json())
+    .then(json => showResults(json))
+
 }
 
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+function getIssues(json) {
+  document.getElementById('issues').innerHTML =
+     `<ul>` + JSON.parse(json).map(
+       i =>
+       '<li>' +
+       `<a href="${i.html_url}">${issue.title}</a>`
+       + '</li>').join()}
 }
