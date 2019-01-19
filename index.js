0440b980a5d2ccdb63e1d105bd2bae1ebdebcda9
arianaberger <ariana.berger@gmail.com>
2019-01-19 13:52:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const token = 'b9151e81def9060c42fb5bfba379e9adce7cdca9';
+ 
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -6,17 +9,60 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(
+  	`https://api.github.com/repos/${repo}/forks`,
+	{
+	  method: 'POST',
+	  headers: {
+	  	Authorization: `token ${token}`
+	  }
+	 }).then(res => 
+	  res.json()).
+	  then(data => showResults(data)
+  );
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+	const url = json.html_url
+	const linkText  = `<a href="${url}" target="_blank">>>View Repo</a>`
+	document.getElementById('results').innerHTML = linkText
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+	const title = document.getElementById('title').value
+	const body = document.getElementById('body').value
+	const issueBody = {
+		title: title,
+		body: body
+	}
+	fetch(
+		'https://api.github.com/repos/arianaberger/js-ajax-fetch-lab/issues',
+		{
+			method: 'POST',
+			body: JSON.stringify(issueBody),
+			headers: {
+				Authorization: `token ${token}`
+			}
+		}
+	).then(res => res.json()).then(data => getIssues())
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+	const issues = fetch(
+		'https://api.github.com/repos/arianaberger/js-ajax-fetch-lab/issues',
+		{
+			method: 'GET',
+			headers: {
+				Authorization: `token ${token}`
+			}
+		}
+	).then(res => res.json()).then(data => showIssues(data))
+}
+
+function showIssues(data){
+	const issueInfo = data.map( i => 
+		`<li>${i.title}</li>
+		<li>${i.body}</li>`).join('');
+	const addHTML = '<ul>' + issueInfo + '</ul>'
+	document.getElementById('issues').innerHTML = addHTML
 }
