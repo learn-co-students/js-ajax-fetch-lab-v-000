2b7fb4b46b57d945af4542a80f83a93d5dfbe14f
Pamela Frieze <pfrieze@gmail.com>
2019-03-19 03:05:44 +0000

    Done.

--- index.js
+++ index.js
@@ -19,12 +19,36 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  fetch('json.html_url')
+  .then(response => response.json())
+  .then(json => console.log(json));
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = '/js-ajax-fetch-lab';
+  const issue = { 
+    body: 'test body'
+  };
+  
+  fetch(`${repo}/issues`, 
+  {
+  method: 'POST',
+  body: JSON.stringify(issue),
+  
+  headers: {
+    Authorization: `token ${getToken}`
+   }
+  })
+  .then(res => console.log(res));
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = '/js-ajax-fetch-lab';
+
+  fetch(`${repo}/issues`)
+  .then(response => response.json())
+  .then(json => console.log(json));
+  
 }
