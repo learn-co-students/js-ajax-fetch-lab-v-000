11eb9102888275f797e1e830da69699f3c739076
Mika88 <mikaspivak@gmail.com>
2019-04-10 18:51:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '';
+  return "";
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+     }
+   }
+ ).then(response => response.json())
+ .then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
-}
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  let body = document.getElementById('body').value
+  let title = document.getElementById('title').value
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: 'POST',
+    body: body,
+    title: title,
+    headers: {
+      Authorization: `token ${getToken}`
+    }
+  }).then(response => response.json());
+
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
