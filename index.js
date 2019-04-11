e72a942357089cf8310fdfa9dea963f11f9a80e0
Jayon Kim <jpkim921@gmail.com>
2019-04-10 22:12:40 -0400

    made comment changes

--- index.js
+++ index.js
@@ -25,30 +25,31 @@ function forkRepo() {
     .then(res => res.json())
     .then(json => showResults(json));
     // .then(json => console.log(json));
-
-
 }
 
+//use this function to display the results from forking via the API
 function showResults(json) {
-  //use this function to display the results from forking via the API
   document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
     json.html_url
   }</a>`;
 }
 
+
+
+//use this function to create an issue based on the values input in index.html
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
   // POST /repos/:owner/:repo/issues
 
   const baseURL = 'https://api.github.com'
   let url = `${baseURL}/repos/jpkim921/js-ajax-fetch-lab/issues`;
 
-
+// we're grabbing the issue title and body from the webpage throught the DOM
   let postData = {
     title: document.getElementById('title').value,
     body: document.getElementById('body').value
   }
 
+// the issues are posted using the POST method...
   fetch(url, {
     method: 'POST',
     body: JSON.stringify(postData),
@@ -57,11 +58,12 @@ function createIssue() {
     }
   })
   .then(res => res.json())
+  // ...then we fetch all the open issues posted.
   .then(json => getIssues());
 }
 
+//once an issue is submitted, fetch all the open issues to see the issues you are creating. used above at the end of createIssue()
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
   const baseURL = 'https://api.github.com';
   const repo = `jpkim921/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
