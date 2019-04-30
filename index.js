9681903f08678b4043fed05d9eaeff4f9fed6b02
George  Pianka <gpianka1@gmail.com>
2019-04-30 08:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 const baseURL = 'https://api.github.com';
 const user = 'NotoriousCottonball';
 const token = '7685cf7c288cd1ae9fe0a4b15cdd191e32923144';
