1055b4e32f5f1516934ca12eb76f6c4719523c34
George  Pianka <gpianka1@gmail.com>
2019-04-30 07:21:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
 }
 
 function createIssue() {
