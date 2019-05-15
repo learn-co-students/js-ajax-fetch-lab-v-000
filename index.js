06080f3fe70987884913a24274cbdf04dca45d5b
harleyharl <harleymteden@gmail.com>
2019-05-15 13:46:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,82 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  // const token = 'd2f9244a729ab0360204bc58137c139bd8ead593'
+  // return token
   return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  token = getToken()
+  fetch(
+    'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${token}`
+      }
+    }
+  ).then(res => res.json())
+  .then(res => showResults(res))
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  const repoLink = '<a href="' + json.html_url + '">' + ` ${json.html_url} </a>`
+  document.getElementById('results').innerHTML = repoLink
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+
+  token = getToken()
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  }
+
+  fetch(
+    'https://api.github.com/repos/harleyharl/js-ajax-fetch-lab/issues',
+    {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        Authorization: `token ${token}`
+      }
+    }
+  ).then(res => res.json())
+  .then(res => {
+    getIssues()
+    })
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  token = getToken()
+
+  const issues = fetch(
+    'https://api.github.com/repos/harleyharl/js-ajax-fetch-lab/issues',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${token}`
+      }
+    }
+  ).then(res => res.json())
+  .then(res => makeList(res))
+
+  function makeList(res){
+    const issueList =
+        '<ul>' +
+        res
+          .map(issue => {
+            return `
+              <li>
+                  <br>${issue.title}</b><br>
+                  ${issue.body}
+              </li>`;
+          })
+          .join('') +
+        '</ul>';
+
+    document.getElementById('issues').innerHTML = issueList
+  }
+
 }
