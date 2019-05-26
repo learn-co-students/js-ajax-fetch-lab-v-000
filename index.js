f01ae9894b8563a667c2ae50fcabeb4a0ab2e245
Aly Alidina <aly_alidina@hotmail.com>
2019-05-26 13:37:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
 const baseURL = 'https://api.github.com';
-const user = 'aalidina'
-
+const user = 'aalidina';
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  //use fetch to fork it!
   const url = `${baseURL}/repos/${repo}/forks`;
-  fetch(url, {
+  fetch(url,{
     method: 'POST',
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
     .then(res => res.json())
-    .then(json => showResults(json));
+    .then(data => showResults(data))
 }
 
 function showResults(json) {
   document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
     json.html_url
   }</a>`;
-}
-
-
-  function createIssue() {
-  const repo = `${user}/js-ajax-fetch-lab`;
-  const url = `${baseURL}/repos/${repo}/issues`;
-  const postData = {
-    title: document.getElementById('title').value,
-    body: document.getElementById('body').value
-  };
 }
 
 function createIssue() {
