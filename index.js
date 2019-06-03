3bda741197d2559450ad8e980a014cb32e5391d2
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-06-03 15:33:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
-function getIssues() {
-  fetch('https://api.github.com/repos/andreanasuto/javascript-fetch-lab/issues', {
-  headers: {
-    Authorization: `token ${getToken()}`
-    }
-  }).then(res => res.json()).then(json => showIssues(json))
-}
-
-function showIssues(json) {
-  const issuesDiv = document.getElementById("issues");
-  const issuesLis = json.map(issue => `<li><strong>${issue.title}</strong> -- ${issue.body}</li>`).join('');
-  issuesDiv.innerHTML = `<ul>${issuesLis}</ul>`;
-}
-
-function createIssue() {
-  const issueData = {
-  title: document.getElementById('title').value,
-  body: document.getElementById('body').value
-  }
-  const url = 'https://api.github.com/repos/andreanasuto/javascript-fetch-lab/issues' //url to post issue
-
-  fetch(url, {
-    // POST /repos/:owner/:repo/issues
-    method: 'post',
-    body: JSON.stringify(issueData),
-    headers: {
-      Authorization: `token ${getToken()}`
-    }
-  }).then(res => res.json())
-}
-
-function showResults(json) {
-  // In showForkedRepo, display the repo information in the browser by appending html with a link to the repository url to the DOM.
-  $("results").append('<a href="${json.html_url}">${json.name}</a>')
-
+function getToken () {
+  return ''
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab/forks'
-  const url = 'https://api.github.com/repos/' + repo
-  //use fetch to fork it!
-  fetch(url, {
-  method: 'post',
-  headers: {
-    'Authorization': 'token' + getToken()
-  }
-}).then(res => res.json()).then(json => showResults(json))
+  const token = getToken()
+  return fetch('https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks', {
+    method: 'post',
+    headers: {
+      "Authorization": `token ${token}`
+    }
+  }).then(res => res.json).then(json => showResults(json))
 }
 
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
+function showResults(json) {
+  document.getElementById('results').append(`<p> ${json.html_url} </p>`)
+}
+
+function createIssue(owner,repo) {
+  const title_issue = document.getElementById('title').value
+  const body_issue = document.getElementById('body').value
+  const postData = {
+    title: title_issue,
+    body: body_issue
+  }
+  const token = getToken()
+  return fetch('https://api.github.com/repos/javascript-fetch-lab/issues', {
+    method: 'post',
+    headers: {
+            Authorization: `token ${token}`,
+        },
+    body: JSON.stringify(postData)
+  }).then(response => response.json())
+}
+
+function getIssues() {
+  const token = getToken()
+  const url = 'https://api.github.com/repos/javascript-fetch-lab/issues'
+
+  fetch(url,{
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json).then(json => displayIssues(json))
+}
+
+function displayIssues(json) {
+  debugger
 }
