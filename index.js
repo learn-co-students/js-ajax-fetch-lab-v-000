6f69eea9f1150e7665ad932d88c5d88f4deaa15b
github username <github email address>
2019-06-04 23:11:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
+const base = "https://api.github.com"
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return '';
 }
-
+//09b2ad94a672cbcfceb9388194b97dcb20fbd98a
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+
+  fetch("https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks",
+  {
+    method: 'POST',
+    headers: {
+      Authorization: `token 09b2ad94a672cbcfceb9388194b97dcb20fbd98a`
+    }
+  }
+).then(res => console.log(res));
+
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  const results = `<a href="${json.html_url}">${json.html_url}</a><br>`
+  document.getElementById('results').innerHTML = results;
+
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  fetch("https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/issues",
+  {
+    method: 'POST',
+    headers: {
+      Authorization: `token 09b2ad94a672cbcfceb9388194b97dcb20fbd98a`
+    },
+    title: `${document.getElementById('title').value}`,
+    body: `${document.getElementById('body').value}`
+  }
+).then(res => console.log(res));)
 }
 
 function getIssues() {
