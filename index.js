e4da5f4b262383d37919ef49c63bda691373803e
MDAM182 <mdam2345@gmail.com>
2019-06-05 12:55:32 -0400

    testing

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+
+
+  const token = '9d8cb2a068d9fd4191d1ddaefb8540eb4f74c4ea';
+
+    fetch(`https://api.github.com/user/repos`, {
+      headers: {
+        Authorization: `token ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+      return '9d8cb2a068d9fd4191d1ddaefb8540eb4f74c4ea';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  // const repo = 'MDAM182/js-ajax-fetch-lab-v-000';
+  // fetch(repo)
+  // .then(resp => resp.json())
+  // .then(json => console.log(json));
+  // //use fetch to fork it!
 }
 
 function showResults(json) {
