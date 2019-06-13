aa1df677ac33d0776b390a7a7867e2a44057fd40
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-12 21:01:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = '';
 
 function getToken() {
   //change to your token to run in browser, but set
@@ -9,17 +9,56 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const url = `${baseURL}/repos/${repo}/forks`
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(response => response.json())
+  .then(json => showResults(json))
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  const result = document.getElementById('results');
+  result.innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
-}
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+
+  const newIssue = {
+    'title': `${title}`,
+    'body': `${body}`
+  }
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(newIssue)
+  })
+  .then(response => response.json())
+  .then(json => getIssues(json))
+};
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${getToken()}`}
+    })
+    .then(response => response.json)
+    .then(json => showIssues(json))
+};
+
+function showIssues(json) {
+  document.getElementById('issues').append( `${json}`)
 }
