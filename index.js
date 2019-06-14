0f13da2ab55febd5679375b00ac1c4cc2c84967f
brennanmrose <brennan.m.rose@gmail.com>
2019-06-13 21:46:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'brennanmrose ';
+const token = '';
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '';
+	fetch('https://api.github.com/brennanmrose/repos', {
+		headers: {
+			Authorization: `token ${token}`,
+			mode:'no-cors'
+		}
+	})
+		.then(res => res.json())
+		.then(json => console.log(json));
+	  // return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const repo = 'https://github.com/learn-co-curriculum/js-ajax-fetch-lab';
+  fetch(repo, { Authorization: `token ${token}`, mode:'no-cors' })
+  	.then(response => response.json())
+  	.then(json => console.log(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+
 }
+  //use this function to display the results from forking via the API
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
@@ -23,3 +33,5 @@ function createIssue() {
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
 }
+
+
