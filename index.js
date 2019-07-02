ee291f387079dcd55f7dea5ae66105cf983b1064
sjwakean <swakeman@ca.rr.com>
2019-07-02 15:04:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'sjwakeman';
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return '';
+  
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks', { //changed url
+  method: 'POST',
+  headers: {
+    Authorization: `token ${getToken()}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
+
 }
 
 function showResults(json) {
@@ -18,7 +28,17 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  fetch('https://api.github.com/repos/${user}/js-ajax-fetch-lab/issues', { //changed url
+  method: 'POST',
+  headers: {
+    Authorization: `token ${getToken()}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
+
 }
+
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
